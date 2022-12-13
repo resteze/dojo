1d2343dedeb0d1ef53c4e65c47000bb856d31bf6
resteze <davideze.eze@gmail.com>
2022-12-13 11:26:29 -0600

    algo

---
+++ algorithms/day2.js
@@ -0,0 +1,16 @@
+// variables is a place holder aka a box
+// we declare new variables with var
+// we reassign old variables with =
+// variables can vary,be reused, and swap out what is inside
+// variables have a label so we know whats inside
+// the computer does not care what the name is as long as its there
+// the var label can not be a keyword(ex: if and for) or non alphabetical characters other than _, start with a int or an uppercase, or have any spaces\
+// naming convention: the first word starts with lowercase and every word after is an uppercase
+// computers are dumb because whatever information u gave it takes at face value and is unable to change it unless told otherwise
+// without data computers have no programs, or "intellect"
+// the 3 data types are:
+// strings: text surronded by either a pair of single quotes or a pair of double quotes(a single quote can have double quotes inside)
+// numbers: whole numbers and decimal numbers
+// boolean: true or false
+//null means nothing
+//undefined means not defined
